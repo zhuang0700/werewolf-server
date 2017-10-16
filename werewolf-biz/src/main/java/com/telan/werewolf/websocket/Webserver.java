3e545e3d81c8328a592cc1f588429ba5796c3404
weiwenliang <weiwenliang@xiaomi.com>
2017-10-16 10:19:40 +0800

    web socket

--- werewolf-biz/src/main/java/com/telan/werewolf/websocket/Webserver.java
+++ werewolf-biz/src/main/java/com/telan/werewolf/websocket/Webserver.java
@@ -10,7 +10,6 @@ import com.telan.werewolf.param.BaseRequestData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.socket.server.standard.SpringConfigurator;
 
 import java.io.IOException;
 import java.util.Map;
